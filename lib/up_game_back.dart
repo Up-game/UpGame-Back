aecdcd0eee1e442ef00e884f3f25386e214551af
Luca <coduri.vitor@gmail.com>
2022-09-15 00:00:43 +0200

    not making menu anymore

--- lib/up_game_back.dart
+++ lib/up_game_back.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-import 'dart:isolate';
+import 'dart:io';
+
 import 'package:dcli/dcli.dart';
 import 'package:netframework/netframework.dart';
 import 'package:up_game_back/up_server.dart';
@@ -23,112 +24,37 @@ enum CliResponse {
 }
 
 void main(List<String> args) async {
-  final ReceivePort receivePort = ReceivePort();
-  final ReceivePort loggingPort = ReceivePort();
-  final isolate = await Isolate.spawn(
-      serverIsolate, [receivePort.sendPort, loggingPort.sendPort]);
-  final SendPort sendPort = await receivePort.first;
-
-  final loggingSubscription = loggingPort.listen((message) {
-    print(message);
-  });
-
-  bool exit = false;
-  while (!exit) {
-    String selected = menu(
-      options: [
-        'Start server',
-        'Stop server',
-        'Restart server',
-        'Show logs',
-        'Exit',
-      ],
-      prompt: 'Your selection:',
-    );
-    print(selected);
-    switch (selected) {
-      case 'Start server':
-        sendPort.send(CliCommands.start);
-        break;
-      case 'Stop server':
-        sendPort.send(CliCommands.stop);
-        break;
-      case 'Restart server':
-        sendPort.send(CliCommands.restart);
-        break;
-      case 'Show logs':
-        sendPort.send(CliCommands.showLog);
-        break;
-      case 'Exit':
-        sendPort.send(CliCommands.exit);
-        exit = true;
-        break;
-    }
-    await Future.delayed(Duration(seconds: 1));
-  }
-  receivePort.close();
-  loggingPort.close();
-  await loggingSubscription.cancel();
-  print('Exiting...');
-}
-
-void serverIsolate(List<SendPort> ports) async {
-  // Setup
-  bool quit = false;
-  SendPort sendPort = ports[0];
-  SendPort loggingPort = ports[1];
-  final ReceivePort receivePort = ReceivePort();
-  sendPort.send(receivePort.sendPort);
-
-  final UpServer server = UpServer(6000, printer: ((level, actor, message) {
+  final UpServer server = UpServer(6000, printer: (level, actor, message) {
     switch (level) {
       case LogLevel.none:
         break;
       case LogLevel.info:
       case LogLevel.debug:
       case LogLevel.verbose:
-        loggingPort.send("[$level][$actor] $message");
+        Terminal().write(green('[$level][$actor] $message'));
         break;
       case LogLevel.error:
-        loggingPort.send("[${red(level.toString())}][$actor] $message");
+        Terminal().write("[${red(level.toString())}][$actor] $message");
         break;
       case LogLevel.warning:
-        loggingPort.send("[${yellow(level.toString())}][$actor] $message");
-        break;
-    }
-  }));
-
-  // listen to Cli commands
-  late final StreamSubscription<dynamic> commandSubscription;
-  commandSubscription = receivePort.listen((message) async {
-    final CliCommands command = message as CliCommands;
-
-    switch (command) {
-      case CliCommands.start:
-        await server.start();
-        break;
-      case CliCommands.stop:
-        await server.stop();
-        break;
-      case CliCommands.restart:
-        await server.start();
-        await server.stop();
-        break;
-      case CliCommands.showLog:
-        print("coucou");
-        break;
-      case CliCommands.hideLog:
-        break;
-      case CliCommands.exit:
-        quit = true;
-        await server.stop();
-        commandSubscription.cancel();
-        receivePort.close();
+        Terminal().write("[${yellow(level.toString())}][$actor] $message");
         break;
     }
   });
 
-  while (!quit) {
+  late final StreamSubscription sigintSub;
+  sigintSub = ProcessSignal.sigint.watch().listen((signal) async {
+    if (signal != ProcessSignal.sigusr1) {
+      print("Killing server !");
+      server.stop();
+    }
+  });
+
+  await server.start();
+
+  while (server.isRunning) {
     await server.update(blocking: true);
   }
+
+  print("left");
 }
